8fb9c5fa3196cebc2968d3142c9eaf23b8e4dd5a
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-07-20 21:40:36 +0200

    Solve task 5, new method to get the total

--- TicketMachine.java
+++ TicketMachine.java
@@ -11,68 +11,76 @@
  */
 public class TicketMachine
 {
-    // The price of a ticket from this machine.
-    private int price;
-    // The amount of money entered by a customer so far.
-    private int balance;
-    // The total amount of money collected by this machine.
-    private int total;
+	// The price of a ticket from this machine.
+	private int price;
+	// The amount of money entered by a customer so far.
+	private int balance;
+	// The total amount of money collected by this machine.
+	private int total;
 
-    /**
-     * Create a machine that issues tickets of the given price.
-     * Note that the price must be greater than zero, and there
-     * are no checks to ensure this.
-     */
-    public TicketMachine(int cost)
-    {
-        price = cost;
-        balance = 0;
-        total = 0;
-    }
-
-    /**
-     * Return the price of a ticket.
-     */
-    public int getPrice()
-    {
-        return price;
-    }
-
-    /**
-     * Return the amount of money already inserted for the
-     * next ticket.
-     */
-    public int getBalance()
-    {
-        return balance;
-    }
-
-    /**
-     * Receive an amount of money from a customer.
-     */
-    public void insertMoney(int amount)
-    {
-        balance = balance + amount;
-    }
-
-    /**
-     * Print a ticket.
-     * Update the total collected and
-     * reduce the balance to zero.
-     */
-    public void printTicket()
-    {
-        // Simulate the printing of a ticket.
+	/**
+	 * Create a machine that issues tickets of the given price.
+	 * Note that the price must be greater than zero, and there
+	 * are no checks to ensure this.
+	 */
+	public TicketMachine(int cost)
+	{
+		price = cost;
+		balance = 0;
+		total = 0;
+	}
+	
+	/**
+	 * Return the price of a ticket.
+	 */
+	public int getPrice()
+	{
+		return price;
+	}
+	
+	/**
+	 * Return the amount of money already inserted for the
+	 * next ticket.
+	 */
+	public int getAmount()
+	{
+		return balance;
+	}
+	
+	/** 
+	 * method get return the value of the total amount inserted.
+	 */
+	public int getTotal()
+	{
+		return total;
+	}
+    
+	/**
+	 * Receive an amount of money from a customer.
+	 */
+	public void insertMoney(int amount)
+	{
+		balance = balance + amount;
+	}
+	
+	/**
+	 * Print a ticket.
+	 * Update the total collected and
+	 * reduce the balance to zero.
+	 */
+	public void printTicket()
+	{
+		// Simulate the printing of a ticket.
         System.out.println("##################");
-        System.out.println("# The BlueJ Line");
-        System.out.println("# Ticket");
-        System.out.println("# " + price + " cents.");
-        System.out.println("##################");
-        System.out.println();
-
-        // Update the total collected with the balance.
-        total = total + balance;
-        // Clear the balance.
-        balance = 0;
-    }
+		System.out.println("# The BlueJ Line");
+		System.out.println("# Ticket");
+		System.out.println("# " + price + " cents.");
+		System.out.println("##################");
+		System.out.println();
+		
+		// Update the total collected with the balance.
+		total = total + balance;
+		// Clear the balance.
+		balance = 0;
+	}
 }
