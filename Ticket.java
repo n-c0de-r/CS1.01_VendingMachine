2aec0a9f72a5d60f5cbe9a11f625401d398d8548
n-c0de-r <s0577683@htw-berlin.de>
2022-07-01 11:04:32 +0200

    Remove redundant code, reset GUI

--- Ticket.java
+++ Ticket.java
@@ -1,10 +1,9 @@
-package main;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * Ticket represents a ticket printed by the TicketMachine.
- * Being it's own object, could use some own methods, too.
+ * Ticket represents a ticket printed by the TicketMachine. Being it's own
+ * object, could use some own methods, too.
  *
  * @author n-c0de-r
  * @version 2021.07.21
@@ -14,36 +13,31 @@ public class Ticket {
 	private String lineName;
 	private int ticketPrice;
 	private int ticketNumber;
-	
+
 	/**
 	 * Constructor of a Ticket.
 	 * 
-	 * @param name	Name of the provider.
-	 * @param price	Price of a single ticket.
-	 * @param ticketnumber	This ticket's number.
+	 * @param name         Name of the provider.
+	 * @param price        Price of a single ticket.
+	 * @param ticketnumber This ticket's number.
 	 */
-	public Ticket (String name, int price, int number) {
+	public Ticket(String name, int price, int number) {
 		// Simulate a ticket, output to the console.
 		lineName = name;
 		ticketPrice = price;
 		ticketNumber = number;
 	}
-		
-		
+
 	public String printTicketInfo() {
-		String str = "##################<br>"
-					+ "# The " + lineName + " Line <br>"
-					+ "# " + ticketPrice + " cents.<br>"
-					+ "##################" + "<br><br>"
-					+ "# Ticket number: "+ lineName + "_" + ticketNumber + "<br><br>";
-		
-		//Set the date format.
+		String str = "##################<br>" + "# The " + lineName + " Line <br>" + "# " + ticketPrice + " cents.<br>"
+				+ "##################" + "<br><br>" + "# Ticket number: " + lineName + "_" + ticketNumber + "<br><br>";
+
+		// Set the date format.
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("yy/MM/dd HH:mm");
 		// Get the current time and add 2 hours to it, ticket is valid that long.
-		str += "Valid until: " + LocalDateTime.now().plusHours(2).format(format) + "<br>"
-					+"##################";
-		
+		str += "Valid until: " + LocalDateTime.now().plusHours(2).format(format) + "<br>" + "##################";
+
 		return str;
 	}
-	
+
 }
